58be65de224a21f31111fb2028a7dc22ac42867b
Othman Andour <andour.otman@gmail.com>
2018-10-01 14:22:53 +0200

    Generated the items directly in the Level class

--- game.py
+++ game.py
@@ -3,15 +3,16 @@
 
 
 import random
+import pygame
 
 START="s"
-FINISH="f"
+FINISH="f" 
 FLOOR="0"
 WALL="1"
 
 LEVEL_HEIGHT=15
 LEVEL_WIDTH=15
-OBJECTS=["NEEDLE","TUBE","ETHER"]
+ITEMS=["NEEDLE","TUBE","ETHER"]
 
 
 
@@ -20,12 +21,22 @@ class Map:
     def __init__(self,level_file):
         self.level_file=level_file
         self.generate_map()
+        #self.generate_objects()
         
     def generate_map(self):
         with open(self.level_file,'r') as f:
             self.map=[list(x)[:-1] for x in f]
-        return self.map
+        print(self.map)
 
+    def generate_items(self):
+        for item in ITEMS:
+            self.x_item=random.randint(0,LEVEL_WIDTH-1)
+            self.y_item=random.randint(0,LEVEL_HEIGHT-1)
+            while self.map[self.x_item][self.y_item] != FLOOR:
+               self.x_item=random.randint(0,LEVEL_WIDTH-1)
+               self.y_item=random.randint(0,LEVEL_HEIGHT-1)
+            self.map[self.x_item][self.y_item]=item
+        print(self.map)
 
 class Hero:
     def __init__(self,x_pos,y_pos,map):
@@ -57,34 +68,12 @@ class Hero:
                     self.x +=1
 
 
-class Object:
-   
-    def __init__(self,map):
-        #self.x_obj=x_obj
-        #self.y_obj=y_obj
-        self.map=map
-        self.generate_objects()
-
-    def __repr__(self):
-        return self.map
-
-    def generate_objects(self):
-        #for obj in OBJECTS:
-        #    self.x_obj=random.randint(0,LEVEL_WIDTH)
-        #    self.y_obj=random.randint(0,LEVEL_HEIGHT)
-        #    while self.map[self.x_obj][self.y_obj] != FLOOR:
-        #       self.x_obj=random.randint(0,LEVEL_WIDTH)
-        #       self.y_obj=random.randint(0,LEVEL_HEIGHT)
-        #    self.map[self.x_obj][self.y_obj]=obj
-        return self.map
-
 
 def main():
+    pygame.init()
+    screen=pygame.display.set_mode(LEVEL_HEIGHT,LEVEL_WIDTH)
     map=Map('level')
-    objects=Object(map)
     
-
-
 
 if __name__=="__main__":
     main()
